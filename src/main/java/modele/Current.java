a5b2645b7456d373cf2801e6eff39f35fa67ffe2
Tom <la248514@student.helha.be>
2026-03-07 10:58:24 +0100

    Ajout de tout les classes

---
+++ src/main/java/modele/Current.java
@@ -0,0 +1,24 @@
+package modele;
+
+public class Current extends StatePlayer{
+
+    // Constructeur
+    Current(Player player) {
+        super(player);
+    }
+
+    @Override
+    public void endTurn() {
+
+    }
+
+    @Override
+    public void movePlayer(int score, int x, int y) {
+
+    }
+
+    @Override
+    public void endVersus() {
+
+    }
+}
---
+++ src/main/java/modele/Question.java
@@ -0,0 +1,33 @@
+package modele;
+
+import java.util.ArrayList;
+
+public class Question {
+    private String question;
+    private String trueAnswer;
+    private int difficulty;
+    private ArrayList<String> answers;
+
+    public Question(String question, String trueAnswer, int difficulty, ArrayList<String> answers) {
+        this.question = question;
+        this.trueAnswer = trueAnswer;
+        this.difficulty = difficulty;
+        this.answers = answers;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getTrueAnswer() {
+        return trueAnswer;
+    }
+
+    public ArrayList<String> getAnswers() {
+        return answers;
+    }
+}
