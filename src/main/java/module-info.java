904486441e6d71e71215a110936e2552b49ba747
CoperProf <williamkestelyn@gmail.com>
2026-03-12 11:11:31 +0100

    ajout de la lecture des questions

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,15 @@
 module com.example.ttmc2026 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
+    // Tu avais déjà celui-ci pour GameBoard
+    opens modele to com.google.gson;
+
+    // AJOUTE CETTE LIGNE pour la classe Question
+    opens modele.question to com.google.gson;
 
     opens com.example.ttmc2026 to javafx.fxml;
     exports com.example.ttmc2026;
+    exports test;
 }
