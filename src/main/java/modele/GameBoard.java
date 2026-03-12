755766d6bfcd25f78b0a08c4b03e085f89014f59
CoperProf <williamkestelyn@gmail.com>
2026-03-12 09:47:26 +0100

    ajout de la lecture des cases

--- src/main/java/modele/GameBoard.java
+++ src/main/java/modele/GameBoard.java
@@ -2,6 +2,10 @@ package modele;
 
 import modele.entity.Entity;
 
+
+import com.google.gson.Gson;
+import modele.question.Theme;
+
 import java.io.File;
 import java.util.ArrayList;
 
@@ -26,6 +30,29 @@ public class GameBoard {
 
     public void createTilde(File json){
         // Ici on va ajouter les cases sauvegardées dans le json
+        Gson gson = new Gson();
+        try (java.io.FileReader reader = new java.io.FileReader(json)) {
+            java.lang.reflect.Type listType = new com.google.gson.reflect.TypeToken<ArrayList<TildeDTO>>(){}.getType();
+            ArrayList<TildeDTO> dtos = gson.fromJson(reader, listType);
+
+            for (TildeDTO dto : dtos) {
+                Theme t = new Theme(dto.theme); // À remplacer par le bon thème
+                Tilde tilde = new Tilde(dto.positionX, dto.positionY, dto.color, t);
+                this.tildes.add(tilde);
+            }
+
+            System.out.println("Chargement réussi : " + tildes.size() + " cases.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class TildeDTO {
+        int positionX;
+        int positionY;
+        String color;
+        String theme;
     }
 
     // Getter
